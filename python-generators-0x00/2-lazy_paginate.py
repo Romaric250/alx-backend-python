e08784fbe4a7df7d8bab03e0e549fe6ab71667db
Romaric Lonfonyuy <78319422+Romaric250@users.noreply.github.com>
2024-12-08 17:27:25 +0000

    task 3 completed and dusted

---
+++ python-generators-0x00/2-lazy_paginate.py
@@ -0,0 +1,31 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def connect_to_prodev():
+    return mysql.connector.connect(
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME')
+    )
+
+def paginate_users(page_size, offset):
+    connection = connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
+
+def lazy_paginate(page_size):
+    offset = 0
+    while True:
+        page = paginate_users(page_size, offset)
+        if not page:
+            break
+        yield page
+        offset += page_size
+
