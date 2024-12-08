de4c908767d0ccf8a8d18559ddd1de427c146256
Romaric Lonfonyuy <78319422+Romaric250@users.noreply.github.com>
2024-12-08 17:42:58 +0000

    task 3 done

---
+++ python-decorators-0x01/2-transactional.py
@@ -0,0 +1,37 @@
+import sqlite3
+import functools
+
+
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        try:
+            result = func(conn, *args, **kwargs)
+        finally:
+            conn.close()
+        return result
+    return wrapper
+
+def transactional(func):
+    @functools.wraps(func)
+    def wrapper(conn, *args, **kwargs):
+        try:
+            result = func(conn, *args, **kwargs)
+            conn.commit()
+        except Exception as e:
+            conn.rollback()
+            raise e
+        return result
+    return wrapper
+
+@with_db_connection
+@transactional
+def update_user_email(conn, user_id, new_email):
+    cursor = conn.cursor()
+    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id))
+
+
+
+update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
